51c669e89b1900b68926452b5c3f030d4d8216fd
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 02:36:58 +0400

    add files

---
+++ src/components/SubContent/SubContent.tsx
@@ -0,0 +1,19 @@
+import Button from "../Button/Button"
+import "./SubContent.css"
+
+function SubContent() {
+  return (
+    <>
+    <div className="box3">
+        <p className="txt7">NEW PRODUCT</p>
+            <h1 className="h112">XX99 Mark II Headphones</h1>
+            <p className="txt22">The new XX99 Mark II headphones is the pinnacle
+               of pristine audio. It redefines your premium headphone experience
+                by reproducing the balanced depth and precision of studio-quality sound.</p>
+            <Button title="SEE PRODUCT"/>
+        </div>
+    </>
+  )
+}
+
+export default SubContent
--- src/pages/HeadphonesPage/HeadphonesPage.tsx
+++ src/pages/HeadphonesPage/HeadphonesPage.tsx
@@ -1,6 +1,6 @@
 import "./HeadphonesPage.css"
 import headphoneImage from "../assets/category-headphones/desktop/image-xx99-mark-two.jpg"
-import Button from "../../components/Button/Button"
+import SubContent from "../../components/SubContent/SubContent"
 
 function HeadphonesPage() {
   return (
@@ -8,16 +8,9 @@ function HeadphonesPage() {
     <div className="mainBox"><center><h1 className="h1">HEADPHONES</h1></center></div>
     <div>
       <img src={headphoneImage} alt="error" width="550px" height="590px" className="img1"/>
-        <div className="box3">
-        <p className="txt7">NEW PRODUCT</p>
-            <h1 className="h112">XX99 Mark II Headphones</h1>
-            <p className="txt22">The new XX99 Mark II headphones is the pinnacle
-               of pristine audio. It redefines your premium headphone experience
-                by reproducing the balanced depth and precision of studio-quality sound.</p>
-            <Button title="SEE PRODUCT"/>
-        </div>
+        <SubContent/>
     </div>
-
+    <div className="content">asdasd</div>
     </>
   )
 }
