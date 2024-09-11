b3caa47e371a05f0b83102327bad11fc20ef5900
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-11 23:08:15 +0400

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -11,7 +11,7 @@ function HomePage() {
           <h1 className="h11">XX99 Mark II Headphones</h1>
           <p className="txt2">Experience natural, lifelike audio and exceptional build quality
           made for the passionate music enthusiast.</p>
-          <button>SEE PRODUCTS</button>
+          <button className="btn">SEE PRODUCTS</button>
 
          </div>
     </div>
