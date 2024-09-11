fc92e18723af6039c518895ca5224cc3d8564bc9
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-11 21:51:07 +0400

    Add styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,10 +6,13 @@ function Navbar() {
     <>
     <div className="navBar">
         <ul className="ul">
-            <li><Link className="li" to="/">HOME</Link></li>
+            <li><Link className="li" to="headphones">AUDIOPHILE</Link></li>
+            <ul className="ul2">
+            <li><Link className="lih" to="/">HOME</Link></li>
             <li><Link className="li" to="headphones">HEADPHONES</Link></li>
             <li><Link className="li" to="speakers">SPEAKERS</Link></li>
             <li><Link className="li" to="eraphones">EARPHONES</Link></li>
+            </ul>
         </ul>
     </div>
     </>
