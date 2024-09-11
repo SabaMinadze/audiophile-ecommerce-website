a0991cd5ddc0025860b5f7b2cc04ee3757648dd6
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-11 22:13:13 +0400

    Add code

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,13 @@
 import "./HomePage.css"
+import heroImage from "./imagehero.jpg"
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+    <div className="mainBox">
+      <img src={heroImage} alt="error" height="609px"/>
+    </div>
+    </>
   )
 }
 
