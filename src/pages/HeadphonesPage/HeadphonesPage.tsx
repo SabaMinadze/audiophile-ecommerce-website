a0fc9a6a8309d88317d598c3aef7896df1232f01
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 00:22:49 +0400

    Add json file

--- src/pages/HeadphonesPage/HeadphonesPage.tsx
+++ src/pages/HeadphonesPage/HeadphonesPage.tsx
@@ -1,9 +1,13 @@
 import "./HeadphonesPage.css"
+import data from ""
 
 function HeadphonesPage() {
   return (
     <>
     <div className="mainBox"><center><h1 className="h1">HEADPHONES</h1></center></div>
+    <div>
+      <img src="" alt="" />
+    </div>
     </>
   )
 }
