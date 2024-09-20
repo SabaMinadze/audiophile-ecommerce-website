3c29adc25b1fe473c82d333f38c5109f78ff1f44
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-20 22:59:25 +0400

    .

--- src/components/SubFooter/SubFooter.tsx
+++ src/components/SubFooter/SubFooter.tsx
@@ -9,6 +9,16 @@ function SubFooter() {
     </div>
     <div className="subFooter2">
         <h1 className="txt11">Bringing you the <span className="txt112">best</span> audio gear</h1>
+        <br /><br />
+        <p className="txtt">Located at the heart of New York City,
+             Audiophile is the premier store for
+              high end headphones, earphones, speakers,
+               and audio accessories. We have a large showroom
+                and luxury demonstration rooms available for you
+                 to browse and experience a wide range of our products.
+                  Stop by our store to meet some of the fantastic people
+                   who make Audiophile the best place to buy your portable
+                    audio equipment.</p>
     </div>
     </>
   )
